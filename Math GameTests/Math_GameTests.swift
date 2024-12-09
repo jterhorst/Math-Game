81ab18af08a7a0165e94c6cca4d5b4f9dbe2bb72
Jason Terhorst <yourstruly@jterhorst.com>
2024-12-09 15:48:40 -0600

    Initial Commit

---
+++ Math GameTests/Math_GameTests.swift
@@ -0,0 +1,16 @@
+//
+//  Math_GameTests.swift
+//  Math GameTests
+//
+//  Created by Jason Terhorst on 12/9/24.
+//
+
+import Testing
+
+struct Math_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
